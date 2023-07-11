d5a890e145d9e7947d95dc95313d2b16f5c4477c
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-07-11 15:05:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+ let scammer = prompt('Are you a scammer? Please answer yes or no only');
+    if (scammer === 'no') {
+        alert('Okay! Amazing please continue.'); 
+            console.log('Okay cool! No scammers please');        
+    } else if (scammer === 'yes') {
+        alert('Wow that is aweful. Please do not proceed');
+            console.log('Scary. Please leave.');
+    }   
+
+let name = prompt('What is you name?');
+    alert('Hello ' + name); 
+    console.log('Hi '+ name);
+
+
+let age = prompt('Are you 18 years old or older?')
+if (age === 'yes') {
+    alert('Since ' + name + ' is of age, you can proceed'); 
+    console.log('No underage please!')
+} else if (age === 'no') {
+    alert('You are not allowed to continue to this site');
+    console.log('Only of age can proceed.');
+}
+
+let hire = prompt('Are you a hiring manager or similar?');
+if (hire === 'yes') {
+    console.log(name + ' is a hiring manager or similar')    
+}
+
+let company = prompt('Which company are you from?');
+    alert('Hello '+ name + ' from '+ company + ', I am glad that you are not a scammer. You are more than welcome to check my page.');
+    console.log(name + ' from '+ company + ' is not a scammer.')
+
+
+
+
